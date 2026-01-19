f87965394f676a37ff4c5f7ff936ee404469ec56
Bastian Prado <bastian.prado.v@gmail.com>
2026-01-19 12:40:11 -0800

    Initial commit

---
+++ src/main/java/com/pmdaiclientrest/PmdAiClientRestApplication.java
@@ -0,0 +1,13 @@
+package com.pmdaiclientrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmdAiClientRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmdAiClientRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmdaiclientrest/PmdAiClientRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmdaiclientrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmdAiClientRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
